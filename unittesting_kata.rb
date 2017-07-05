e6a400c6e83f1f7078ff4ef98c1b80df4920639c
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-07-05 15:08:32 -0400

    a unit test of minedminds 3535, updated

--- unittesting_kata.rb
+++ unittesting_kata.rb
@@ -11,5 +11,9 @@ class Minedminds < Minitest::Test
 	def test_3_returns_mined
 		assert_equal("mined",mined_minds(3))
 	end
+	def test_5_returns_minds
+		assert_equal("minds",mined_minds(5))
+	end
+
 end
 
