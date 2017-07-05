010645621aeef306e301895cd03b9a78cc3e4094
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-07-05 15:03:47 -0400

    a unit test of minedminds 3535

---
+++ unittesting_kata.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require_relative "unittest_one.rb"
+
+class Minedminds < Minitest::Test
+	def test_1_returns_1
+		assert_equal(1,mined_minds(1))
+	end
+	def test_2_returns_2
+		assert_equal(2,mined_minds(2))
+	end
+	def test_3_returns_mined
+		assert_equal("mined",mined_minds(3))
+	end
+end
+
