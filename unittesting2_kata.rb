fad8265681fdfdd3ad74a326cfad9edd5d565383
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-07-06 15:03:45 -0400

    finishing up the homework.

--- unittesting2_kata.rb
+++ unittesting2_kata.rb
@@ -10,13 +10,25 @@ class Minedminds_array < Minitest::Test
 		assert_equal(1,array[0])
 	end
 	
-	def test_3rd_number
-		assert_equal("mined",array[2])
-	end
+	# def test_3rd_number
+	# 	assert_equal("mined",array[2])
+	# end
 
 	def test_6th_number
+		p array
 		assert_equal("mined",array[5])
 	end
+
+	def test_10th_number
+		p array
+		assert_equal("minds",array[9])
+	end
+
+	def test_number_15
+		p array 
+		assert_equal("mined minds",array[14])
+	end
+
 end
 
 
